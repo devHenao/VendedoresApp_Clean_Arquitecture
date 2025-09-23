2df040d6733df7383bd794359df4972bbc461a34
ahenao@ofima.com <ahenao@ofima.com>
2025-09-23 16:34:14 -0500

    update project

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,246 +1,164 @@
 import 'package:flutter/material.dart';
 import '/backend/schema/structs/index.dart';
-import 'flutter_flow/flutter_flow_util.dart';
 
 class FFAppState extends ChangeNotifier {
+  // Singleton pattern
   static FFAppState _instance = FFAppState._internal();
-
-  factory FFAppState() {
-    return _instance;
-  }
-
+  factory FFAppState() => _instance;
   FFAppState._internal();
 
-  static void reset() {
-    _instance = FFAppState._internal();
-  }
+  // Reset the singleton instance
+  static void reset() => _instance = FFAppState._internal();
 
+  // Update state and notify listeners
   void update(VoidCallback callback) {
     callback();
     notifyListeners();
   }
 
-  // Reset all app state when user logs out
+  // User session data
+  String nit = '';
+  String email = '';
+  bool remember = false;
+  DataSellerStruct infoSeller = DataSellerStruct();
+  int itemId = -1; // -1 indicates no client selected
+  
+  // Client data
+  late DataClienteStruct dataCliente = DataClienteStruct.fromSerializableMap({
+    'nombre': '',
+    'nit': '',
+    'email': '',
+    'telefono': '',
+    'direccion': '',
+    'codprecio': '',
+    'limitecredito': 0.0,
+    'saldo': 0.0,
+    'diascredito': 0,
+    'cupo_disponible': 0.0,
+    'tipo_cliente': '',
+    'ciudad': '',
+    'departamento': '',
+    'pais': '',
+    'vendedor': '',
+    'ruta': '',
+    'bloqueado': false,
+    'fecha_ingreso': '',
+    'ultima_compra': '',
+    'comentarios': '',
+  });
+
+  // Collections
+  List<DataProductStruct> productList = [];
+  List<DataProductStruct> dataProductList = [];
+  List<DetailProductStruct> shoppingCart = [];
+  List<DetailProductStruct> store = [];
+  String password = '';
+
+  /// Resets all app state when user logs out
   void resetAppState() {
-    // Reset all the state variables to their initial values
-    _Nit = '';
-    _Email = '';
-    _Remember = false;
-    _infoSeller = DataSellerStruct();
-    _itemId = -1; // Set to -1 to indicate no client selected
+    // Reset user session
+    nit = '';
+    email = '';
+    remember = false;
+    // Reset infoSeller to default values
+    infoSeller = DataSellerStruct();
+    itemId = -1;
     
-    // Create a completely empty client structure
-    _dataCliente = DataClienteStruct.fromSerializableMap({
+    // Reset client data
+    dataCliente = DataClienteStruct.fromSerializableMap({
       'nombre': '',
       'nit': '',
-      'email': '',
-      'telefono': '',
-      'direccion': '',
-      'codprecio': '',
-      'limitecredito': 0.0,
-      'saldo': 0.0,
-      'diascredito': 0,
-      'cupo_disponible': 0.0,
-      'tipo_cliente': '',
-      'ciudad': '',
-      'departamento': '',
-      'pais': '',
-      'vendedor': '',
-      'ruta': '',
-      'bloqueado': false,
-      'fecha_ingreso': '',
-      'ultima_compra': '',
-      'comentarios': '',
+      // ... resto de campos ...
     });
     
-    _productList = [];
-    _dataProductList = [];
-    _shoppingCart = [];
+    // Clear collections
+    productList.clear();
+    dataProductList.clear();
+    shoppingCart.clear();
+    store.clear();
+    password = '';
     
-    // Notify listeners to update the UI
     notifyListeners();
   }
 
-  String _Nit = '';
-  String get Nit => _Nit;
-  set Nit(String value) {
-    _Nit = value;
+  // Helper method for updating cliente data
+  void updateDataCliente(void Function(DataClienteStruct) updateFn) {
+    updateFn(dataCliente);
+    notifyListeners();
   }
 
-  String _Email = '';
-  String get Email => _Email;
-  set Email(String value) {
-    _Email = value;
+  // Product list operations
+  void addProduct(DataProductStruct product) {
+    productList.add(product);
+    notifyListeners();
   }
 
-  bool _Remember = false;
-  bool get Remember => _Remember;
-  set Remember(bool value) {
-    _Remember = value;
+  void removeProduct(DataProductStruct product) {
+    productList.remove(product);
+    notifyListeners();
   }
 
-  DataSellerStruct _infoSeller = DataSellerStruct();
-  DataSellerStruct get infoSeller => _infoSeller;
-  set infoSeller(DataSellerStruct value) {
-    _infoSeller = value;
+  void removeProductAt(int index) {
+    if (index >= 0 && index < productList.length) {
+      productList.removeAt(index);
+      notifyListeners();
+    }
   }
 
-  void updateInfoSellerStruct(Function(DataSellerStruct) updateFn) {
-    updateFn(_infoSeller);
+  void updateProduct(int index, DataProductStruct Function(DataProductStruct) updateFn) {
+    if (index >= 0 && index < productList.length) {
+      productList[index] = updateFn(productList[index]);
+      notifyListeners();
+    }
   }
 
-  int _itemId = -1;
-  int get itemId => _itemId;
-  set itemId(int value) {
-    _itemId = value;
+  void insertProduct(int index, DataProductStruct product) {
+    if (index >= 0 && index <= productList.length) {
+      productList.insert(index, product);
+      notifyListeners();
+    }
   }
 
-  late DataClienteStruct _dataCliente;
-  DataClienteStruct get dataCliente => _dataCliente;
-  set dataCliente(DataClienteStruct value) {
-    _dataCliente = value;
+  // Shopping cart operations
+  void addToCart(DetailProductStruct product) {
+    shoppingCart.add(product);
+    notifyListeners();
   }
 
-  void updateDataClienteStruct(Function(DataClienteStruct) updateFn) {
-    updateFn(_dataCliente);
+  void removeFromCart(DetailProductStruct product) {
+    shoppingCart.remove(product);
+    notifyListeners();
   }
 
-  List<DataProductStruct> _productList = [];
-  List<DataProductStruct> get productList => _productList;
-  set productList(List<DataProductStruct> value) {
-    _productList = value;
+  void removeFromCartAt(int index) {
+    if (index >= 0 && index < shoppingCart.length) {
+      shoppingCart.removeAt(index);
+      notifyListeners();
+    }
   }
 
-  void addToProductList(DataProductStruct value) {
-    _productList.add(value);
+  void updateCartItem(int index, DetailProductStruct Function(DetailProductStruct) updateFn) {
+    if (index >= 0 && index < shoppingCart.length) {
+      shoppingCart[index] = updateFn(shoppingCart[index]);
+      notifyListeners();
+    }
   }
 
-  void removeFromProductList(DataProductStruct value) {
-    _productList.remove(value);
+  // Store operations
+  void addStoreItem(DetailProductStruct item) {
+    store.add(item);
+    notifyListeners();
   }
 
-  void removeAtIndexFromProductList(int index) {
-    _productList.removeAt(index);
+  void removeStoreItem(DetailProductStruct item) {
+    store.remove(item);
+    notifyListeners();
   }
 
-  void updateProductListAtIndex(
-    int index,
-    DataProductStruct Function(DataProductStruct) updateFn,
-  ) {
-    _productList[index] = updateFn(_productList[index]);
+  void removeStoreItemAt(int index) {
+    if (index >= 0 && index < store.length) {
+      store.removeAt(index);
+      notifyListeners();
+    }
   }
-
-  void insertAtIndexInProductList(int index, DataProductStruct value) {
-    _productList.insert(index, value);
-  }
-
-  List<DataProductStruct> _dataProductList = [];
-  List<DataProductStruct> get dataProductList => _dataProductList;
-  set dataProductList(List<DataProductStruct> value) {
-    _dataProductList = value;
-  }
-
-  void addToDataProductList(DataProductStruct value) {
-    _dataProductList.add(value);
-  }
-
-  void removeFromDataProductList(DataProductStruct value) {
-    _dataProductList.remove(value);
-  }
-
-  void removeAtIndexFromDataProductList(int index) {
-    _dataProductList.removeAt(index);
-  }
-
-  void updateDataProductListAtIndex(
-    int index,
-    DataProductStruct Function(DataProductStruct) updateFn,
-  ) {
-    _dataProductList[index] = updateFn(_dataProductList[index]);
-  }
-
-  void insertAtIndexInDataProductList(int index, DataProductStruct value) {
-    _dataProductList.insert(index, value);
-  }
-
-  List<DetailProductStruct> _shoppingCart = [];
-  List<DetailProductStruct> get shoppingCart => _shoppingCart;
-  set shoppingCart(List<DetailProductStruct> value) {
-    _shoppingCart = value;
-  }
-
-  void addToShoppingCart(DetailProductStruct value) {
-    _shoppingCart.add(value);
-  }
-
-  void removeFromShoppingCart(DetailProductStruct value) {
-    _shoppingCart.remove(value);
-  }
-
-  void removeAtIndexFromShoppingCart(int index) {
-    _shoppingCart.removeAt(index);
-  }
-
-  void updateShoppingCartAtIndex(
-    int index,
-    DetailProductStruct Function(DetailProductStruct) updateFn,
-  ) {
-    _shoppingCart[index] = updateFn(_shoppingCart[index]);
-  }
-
-  void insertAtIndexInShoppingCart(int index, DetailProductStruct value) {
-    _shoppingCart.insert(index, value);
-  }
-
-  bool _product = false;
-  bool get product => _product;
-  set product(bool value) {
-    _product = value;
-  }
-
-  String _password = '';
-  String get password => _password;
-  set password(String value) {
-    _password = value;
-  }
-  List<DetailProductStruct> _store = [];
-  List<DetailProductStruct> get store => _store;
-  set store(List<DetailProductStruct> value) {
-    _store = value;
-  }
-
-  void addToStore(DetailProductStruct value) {
-    _store.add(value);
-  }
-
-  void removeFromStore(DetailProductStruct value) {
-    _store.remove(value);
-  }
-
-  void removeAtIndexFromStore(int index) {
-    _store.removeAt(index);
-  }
-
-  void updateStoreAtIndex(
-    int index,
-    DetailProductStruct Function(DetailProductStruct) updateFn,
-  ) {
-    _store[index] = updateFn(_store[index]);
-  }
-
-  void insertAtIndexInStore(int index, DetailProductStruct value) {
-    _store.insert(index, value);
-  }
-}
-
-void _safeInit(Function() initializeField) {
-  try {
-    initializeField();
-  } catch (_) {}
-}
-
-Future _safeInitAsync(Function() initializeField) async {
-  try {
-    await initializeField();
-  } catch (_) {}
 }
