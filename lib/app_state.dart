3ff1e7dd747f3dd5bc1ef772f60a56720d105757
ahenao@ofima.com <ahenao@ofima.com>
2025-09-23 15:58:25 -0500

    delete persistence state in sesion

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import '/backend/schema/structs/index.dart';
-import '/backend/api_requests/api_manager.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'flutter_flow/flutter_flow_util.dart';
 
 class FFAppState extends ChangeNotifier {
@@ -16,107 +14,6 @@ class FFAppState extends ChangeNotifier {
   static void reset() {
     _instance = FFAppState._internal();
   }
-
-  // Future initializePersistedState() async {
-  //   prefs = await SharedPreferences.getInstance();
-  //   _safeInit(() {
-  //     _Nit = prefs.getString('ff_Nit') ?? _Nit;
-  //   });
-  //   _safeInit(() {
-  //     _Email = prefs.getString('ff_Email') ?? _Email;
-  //   });
-  //   _safeInit(() {
-  //     _Remember = prefs.getBool('ff_Remember') ?? _Remember;
-  //   });
-  //   _safeInit(() {
-  //     if (prefs.containsKey('ff_infoSeller')) {
-  //       try {
-  //         final serializedData = prefs.getString('ff_infoSeller') ?? '{}';
-  //         _infoSeller =
-  //             DataSellerStruct.fromSerializableMap(jsonDecode(serializedData));
-  //       } catch (e) {
-  //         print("Can't decode persisted data type. Error: $e.");
-  //       }
-  //     }
-  //   });
-  //   _safeInit(() {
-  //     _itemId = prefs.getInt('ff_itemId') ?? _itemId;
-  //   });
-  //   _safeInit(() {
-  //     if (prefs.containsKey('ff_dataCliente')) {
-  //       try {
-  //         final serializedData = prefs.getString('ff_dataCliente') ?? '{}';
-  //         _dataCliente =
-  //             DataClienteStruct.fromSerializableMap(jsonDecode(serializedData));
-  //       } catch (e) {
-  //         print("Can't decode persisted data type. Error: $e.");
-  //       }
-  //     }
-  //   });
-  //   _safeInit(() {
-  //     _productList = prefs
-  //             .getStringList('ff_productList')
-  //             ?.map((x) {
-  //               try {
-  //                 return DataProductStruct.fromSerializableMap(jsonDecode(x));
-  //               } catch (e) {
-  //                 print("Can't decode persisted data type. Error: $e.");
-  //                 return null;
-  //               }
-  //             })
-  //             .withoutNulls
-  //             .toList() ??
-  //         _productList;
-  //   });
-  //   _safeInit(() {
-  //     _dataProductList = prefs
-  //             .getStringList('ff_dataProductList')
-  //             ?.map((x) {
-  //               try {
-  //                 return DataProductStruct.fromSerializableMap(jsonDecode(x));
-  //               } catch (e) {
-  //                 print("Can't decode persisted data type. Error: $e.");
-  //                 return null;
-  //               }
-  //             })
-  //             .withoutNulls
-  //             .toList() ??
-  //         _dataProductList;
-  //   });
-  //   _safeInit(() {
-  //     _shoppingCart = prefs
-  //             .getStringList('ff_shoppingCart')
-  //             ?.map((x) {
-  //               try {
-  //                 return DetailProductStruct.fromSerializableMap(jsonDecode(x));
-  //               } catch (e) {
-  //                 print("Can't decode persisted data type. Error: $e.");
-  //                 return null;
-  //               }
-  //             })
-  //             .withoutNulls
-  //             .toList() ??
-  //         _shoppingCart;
-  //   });
-  //   _safeInit(() {
-  //     _password = prefs.getString('ff_password') ?? _password;
-  //   });
-  //   _safeInit(() {
-  //     _store = prefs
-  //             .getStringList('ff_store')
-  //             ?.map((x) {
-  //               try {
-  //                 return DetailProductStruct.fromSerializableMap(jsonDecode(x));
-  //               } catch (e) {
-  //                 print("Can't decode persisted data type. Error: $e.");
-  //                 return null;
-  //               }
-  //             })
-  //             .withoutNulls
-  //             .toList() ??
-  //         _store;
-  //   });
-  // }
 
   void update(VoidCallback callback) {
     callback();
@@ -160,200 +57,139 @@ class FFAppState extends ChangeNotifier {
     _dataProductList = [];
     _shoppingCart = [];
     
-    // Clear all persisted data from SharedPreferences
-    prefs.remove('ff_Nit');
-    prefs.remove('ff_Email');
-    prefs.remove('ff_Remember');
-    prefs.remove('ff_infoSeller');
-    prefs.remove('ff_itemId');
-    prefs.remove('ff_dataCliente');
-    prefs.remove('ff_productList');
-    prefs.remove('ff_dataProductList');
-    prefs.remove('ff_shoppingCart');
-    
-    // Force save the empty client data
-    dataCliente = _dataCliente;
-    
     // Notify listeners to update the UI
     notifyListeners();
   }
-
-  late SharedPreferences prefs;
 
   String _Nit = '';
   String get Nit => _Nit;
   set Nit(String value) {
     _Nit = value;
-    prefs.setString('ff_Nit', value);
   }
 
   String _Email = '';
   String get Email => _Email;
   set Email(String value) {
     _Email = value;
-    prefs.setString('ff_Email', value);
   }
 
   bool _Remember = false;
   bool get Remember => _Remember;
   set Remember(bool value) {
     _Remember = value;
-    prefs.setBool('ff_Remember', value);
   }
 
   DataSellerStruct _infoSeller = DataSellerStruct();
   DataSellerStruct get infoSeller => _infoSeller;
   set infoSeller(DataSellerStruct value) {
     _infoSeller = value;
-    prefs.setString('ff_infoSeller', value.serialize());
   }
 
   void updateInfoSellerStruct(Function(DataSellerStruct) updateFn) {
     updateFn(_infoSeller);
-    prefs.setString('ff_infoSeller', _infoSeller.serialize());
   }
 
   int _itemId = -1;
   int get itemId => _itemId;
   set itemId(int value) {
     _itemId = value;
-    prefs.setInt('ff_itemId', value);
   }
 
-  DataClienteStruct _dataCliente =
-      DataClienteStruct.fromSerializableMap(jsonDecode('{}'));
+  late DataClienteStruct _dataCliente;
   DataClienteStruct get dataCliente => _dataCliente;
   set dataCliente(DataClienteStruct value) {
     _dataCliente = value;
-    prefs.setString('ff_dataCliente', value.serialize());
   }
 
   void updateDataClienteStruct(Function(DataClienteStruct) updateFn) {
     updateFn(_dataCliente);
-    prefs.setString('ff_dataCliente', _dataCliente.serialize());
   }
 
   List<DataProductStruct> _productList = [];
   List<DataProductStruct> get productList => _productList;
   set productList(List<DataProductStruct> value) {
     _productList = value;
-    prefs.setStringList(
-        'ff_productList', value.map((x) => x.serialize()).toList());
   }
 
   void addToProductList(DataProductStruct value) {
-    productList.add(value);
-    prefs.setStringList(
-        'ff_productList', _productList.map((x) => x.serialize()).toList());
+    _productList.add(value);
   }
 
   void removeFromProductList(DataProductStruct value) {
-    productList.remove(value);
-    prefs.setStringList(
-        'ff_productList', _productList.map((x) => x.serialize()).toList());
+    _productList.remove(value);
   }
 
   void removeAtIndexFromProductList(int index) {
-    productList.removeAt(index);
-    prefs.setStringList(
-        'ff_productList', _productList.map((x) => x.serialize()).toList());
+    _productList.removeAt(index);
   }
 
   void updateProductListAtIndex(
     int index,
     DataProductStruct Function(DataProductStruct) updateFn,
   ) {
-    productList[index] = updateFn(_productList[index]);
-    prefs.setStringList(
-        'ff_productList', _productList.map((x) => x.serialize()).toList());
+    _productList[index] = updateFn(_productList[index]);
   }
 
   void insertAtIndexInProductList(int index, DataProductStruct value) {
-    productList.insert(index, value);
-    prefs.setStringList(
-        'ff_productList', _productList.map((x) => x.serialize()).toList());
+    _productList.insert(index, value);
   }
 
   List<DataProductStruct> _dataProductList = [];
   List<DataProductStruct> get dataProductList => _dataProductList;
   set dataProductList(List<DataProductStruct> value) {
     _dataProductList = value;
-    prefs.setStringList(
-        'ff_dataProductList', value.map((x) => x.serialize()).toList());
   }
 
   void addToDataProductList(DataProductStruct value) {
-    dataProductList.add(value);
-    prefs.setStringList('ff_dataProductList',
-        _dataProductList.map((x) => x.serialize()).toList());
+    _dataProductList.add(value);
   }
 
   void removeFromDataProductList(DataProductStruct value) {
-    dataProductList.remove(value);
-    prefs.setStringList('ff_dataProductList',
-        _dataProductList.map((x) => x.serialize()).toList());
+    _dataProductList.remove(value);
   }
 
   void removeAtIndexFromDataProductList(int index) {
-    dataProductList.removeAt(index);
-    prefs.setStringList('ff_dataProductList',
-        _dataProductList.map((x) => x.serialize()).toList());
+    _dataProductList.removeAt(index);
   }
 
   void updateDataProductListAtIndex(
     int index,
     DataProductStruct Function(DataProductStruct) updateFn,
   ) {
-    dataProductList[index] = updateFn(_dataProductList[index]);
-    prefs.setStringList('ff_dataProductList',
-        _dataProductList.map((x) => x.serialize()).toList());
+    _dataProductList[index] = updateFn(_dataProductList[index]);
   }
 
   void insertAtIndexInDataProductList(int index, DataProductStruct value) {
-    dataProductList.insert(index, value);
-    prefs.setStringList('ff_dataProductList',
-        _dataProductList.map((x) => x.serialize()).toList());
+    _dataProductList.insert(index, value);
   }
 
   List<DetailProductStruct> _shoppingCart = [];
   List<DetailProductStruct> get shoppingCart => _shoppingCart;
   set shoppingCart(List<DetailProductStruct> value) {
     _shoppingCart = value;
-    prefs.setStringList(
-        'ff_shoppingCart', value.map((x) => x.serialize()).toList());
   }
 
   void addToShoppingCart(DetailProductStruct value) {
-    shoppingCart.add(value);
-    prefs.setStringList(
-        'ff_shoppingCart', _shoppingCart.map((x) => x.serialize()).toList());
+    _shoppingCart.add(value);
   }
 
   void removeFromShoppingCart(DetailProductStruct value) {
-    shoppingCart.remove(value);
-    prefs.setStringList(
-        'ff_shoppingCart', _shoppingCart.map((x) => x.serialize()).toList());
+    _shoppingCart.remove(value);
   }
 
   void removeAtIndexFromShoppingCart(int index) {
-    shoppingCart.removeAt(index);
-    prefs.setStringList(
-        'ff_shoppingCart', _shoppingCart.map((x) => x.serialize()).toList());
+    _shoppingCart.removeAt(index);
   }
 
   void updateShoppingCartAtIndex(
     int index,
     DetailProductStruct Function(DetailProductStruct) updateFn,
   ) {
-    shoppingCart[index] = updateFn(_shoppingCart[index]);
-    prefs.setStringList(
-        'ff_shoppingCart', _shoppingCart.map((x) => x.serialize()).toList());
+    _shoppingCart[index] = updateFn(_shoppingCart[index]);
   }
 
   void insertAtIndexInShoppingCart(int index, DetailProductStruct value) {
-    shoppingCart.insert(index, value);
-    prefs.setStringList(
-        'ff_shoppingCart', _shoppingCart.map((x) => x.serialize()).toList());
+    _shoppingCart.insert(index, value);
   }
 
   bool _product = false;
@@ -366,42 +202,34 @@ class FFAppState extends ChangeNotifier {
   String get password => _password;
   set password(String value) {
     _password = value;
-    prefs.setString('ff_password', value);
   }
-
   List<DetailProductStruct> _store = [];
   List<DetailProductStruct> get store => _store;
   set store(List<DetailProductStruct> value) {
     _store = value;
-    prefs.setStringList('ff_store', value.map((x) => x.serialize()).toList());
   }
 
   void addToStore(DetailProductStruct value) {
-    store.add(value);
-    prefs.setStringList('ff_store', _store.map((x) => x.serialize()).toList());
+    _store.add(value);
   }
 
   void removeFromStore(DetailProductStruct value) {
-    store.remove(value);
-    prefs.setStringList('ff_store', _store.map((x) => x.serialize()).toList());
+    _store.remove(value);
   }
 
   void removeAtIndexFromStore(int index) {
-    store.removeAt(index);
-    prefs.setStringList('ff_store', _store.map((x) => x.serialize()).toList());
+    _store.removeAt(index);
   }
 
   void updateStoreAtIndex(
     int index,
     DetailProductStruct Function(DetailProductStruct) updateFn,
   ) {
-    store[index] = updateFn(_store[index]);
-    prefs.setStringList('ff_store', _store.map((x) => x.serialize()).toList());
+    _store[index] = updateFn(_store[index]);
   }
 
   void insertAtIndexInStore(int index, DetailProductStruct value) {
-    store.insert(index, value);
-    prefs.setStringList('ff_store', _store.map((x) => x.serialize()).toList());
+    _store.insert(index, value);
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
 import 'package:provider/provider.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_web_plugins/url_strategy.dart';
-
 import 'auth/custom_auth/auth_util.dart';
 import 'auth/custom_auth/custom_auth_user_provider.dart';
-
 import '/flutter_flow/flutter_flow_theme.dart';
 import 'flutter_flow/flutter_flow_util.dart';
-import 'flutter_flow/nav/nav.dart';
-import 'index.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
@@ -68,7 +61,7 @@ class _MyAppState extends State<MyApp> {
       });
 
     Future.delayed(
-      Duration(milliseconds: 1000),
+      const Duration(milliseconds: 1000),
       () => _appStateNotifier.stopShowingSplashImage(),
     );
   }
@@ -82,7 +75,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'AppVendedores',
-      localizationsDelegates: [
+      localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
