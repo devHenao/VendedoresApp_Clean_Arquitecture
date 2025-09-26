2b55198ea2427cf17960853e9935dc91086e87b1
ahenao@ofima.com <ahenao@ofima.com>
2025-09-26 15:32:54 -0500

    update

--- lib/modules/clients/infraestructure/repositories/client_repository_impl.dart
+++ lib/modules/clients/infraestructure/repositories/client_repository_impl.dart
@@ -1,5 +1,4 @@
 import 'package:dartz/dartz.dart';
-
 import 'package:app_vendedores/core/errors/exceptions.dart';
 import 'package:app_vendedores/core/errors/failures.dart';
 import 'package:app_vendedores/core/network/network_info.dart';
