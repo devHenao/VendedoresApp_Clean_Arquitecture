3171ea80225a863a0fd7a04e4db29cffbd416fe2
ahenao@ofima.com <ahenao@ofima.com>
2025-10-10 14:38:52 -0500

    Update

--- lib/modules/products/presentation/pages/product_page.dart
+++ lib/modules/products/presentation/pages/product_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
+import 'package:app_vendedores/modules/auth/infrastructure/services/auth_util.dart';
 import 'package:app_vendedores/modules/products/presentation/bloc/product_bloc.dart';
 import 'package:app_vendedores/modules/products/presentation/bloc/product_event.dart';
 import 'package:app_vendedores/modules/products/presentation/widgets/product_view.dart';
+import 'package:app_vendedores/shared/menu/menu_widgets.dart';
 import 'package:app_vendedores/injection_container.dart';
 
 class ProductPage extends StatelessWidget {
@@ -12,6 +15,27 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Productos'),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            const MenuHeader(),
+            const MenuItems(),
+            const Spacer(),
+            MenuActions(onSignOut: () async {
+              await authManager.signOut();
+              if (context.mounted) {
+                GoRouter.of(context).go('/');
+              }
+            }),
+            const SizedBox(height: 20),
+            const MenuFooter(),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
       body: BlocProvider(
         create: (_) => getIt<ProductBloc>()..add(const LoadProducts(codprecio: '')),
         child: const ProductView(),
